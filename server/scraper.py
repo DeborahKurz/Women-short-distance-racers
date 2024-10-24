0333c7210cd1cf9468281ce832b8189acad70df2
Deborah Kurz <DeborahKurz6@gmail.com>
2024-10-24 11:30:26 -0600

    Scraper.py updated based on DRY code recommendations.

--- server/scraper.py
+++ server/scraper.py
@@ -1,88 +1,41 @@
 from bs4 import BeautifulSoup
 import requests
 
-headers = {'use-agent':'my-app/0.0.1'}
 
-#100m dash athlete information
-html_100m = requests.get("https://worldathletics.org/world-rankings/100m/women?regionType=countries&region=usa&page=1&rank", headers=headers)
+# #100m dash athlete information
+# html_100m = requests.get("https://worldathletics.org/world-rankings/{race_distance}/women?regionType=countries&region=usa&page=1&rank", headers=headers)
 
-doc_100m = BeautifulSoup(html_100m.text, 'html.parser')
+def athletes(race_distance):
+    url = f"https://worldathletics.org/world-rankings/{race_distance}/women?regionType=countries&region=usa&page=1&rank"
+    headers = {'use-agent':'my-app/0.0.1'}
 
-athletes_100m_raw = doc_100m.select('.table-row--hover')[:5]
-athletes_100m_data = []
+    html = requests.get(url, headers = headers)
 
-for athlete in athletes_100m_raw:
-    rank = athlete.select_one('td[data-th="Rank"]').get_text(strip=True)
-    name = athlete.select_one('td[data-th="Competitor"]').get_text(strip=True)
-    dob = athlete.select_one('td[data-th="DOB"]').get_text(strip=True)
+    doc = BeautifulSoup(html.text, 'html.parser')
+
+    athletes_raw = doc.select('.table-row--hover')[:5]
+    athletes_data = []
+
+    for athlete in athletes_raw:
+        rank = athlete.select_one('td[data-th="Rank"]').get_text(strip=True)
+        name = athlete.select_one('td[data-th="Competitor"]').get_text(strip=True)
+        dob = athlete.select_one('td[data-th="DOB"]').get_text(strip=True)
+
+        athlete_info = {
+        'Rank': rank,
+        'Name': name,
+        'DOB': dob
+        }
     
-    athlete_info = {
-        'Rank': rank,
-        'Name': name,
-        'DOB': dob
-    }
-    
-    athletes_100m_data.append(athlete_info)
+        athletes_data.append(athlete_info)
 
-#200m dash athlete information
-html_200m = requests.get("https://worldathletics.org/world-rankings/200m/women?regionType=countries&region=usa&page=1&rank", headers=headers)
-
-doc_200m = BeautifulSoup(html_200m.text, 'html.parser')
-athletes_200m_raw = doc_200m.select('.table-row--hover')[:5]
-athletes_200m_data = []
-
-for athlete in athletes_200m_raw:
-    rank = athlete.select_one('td[data-th="Rank"]').get_text(strip=True)
-    name = athlete.select_one('td[data-th="Competitor"]').get_text(strip=True)
-    dob = athlete.select_one('td[data-th="DOB"]').get_text(strip=True)
-
-    athlete_info = {
-        'Rank': rank,
-        'Name': name,
-        'DOB': dob
-    }
-
-    athletes_200m_data.append(athlete_info)
-
-#400m dash athlete information
-html_400m = requests.get("https://worldathletics.org/world-rankings/400m/women?regionType=countries&region=usa&page=1&rank", headers=headers)
-
-doc_400m = BeautifulSoup(html_400m.text, 'html.parser')
-athletes_400m_raw = doc_400m.select('.table-row--hover')[:5]
-athletes_400m_data = []
-
-for athlete in athletes_400m_raw:
-    rank = athlete.select_one('td[data-th="Rank"]').get_text(strip=True)
-    name = athlete.select_one('td[data-th="Competitor"]').get_text(strip=True)
-    dob = athlete.select_one('td[data-th="DOB"]').get_text(strip=True)
-
-    athlete_info = {
-        'Rank': rank,
-        'Name': name,
-        'DOB': dob
-    }
-
-    athletes_400m_data.append(athlete_info)
+    return athletes_data
 
 
-#800m dash athlete information
-html_800m = requests.get("https://worldathletics.org/world-rankings/800m/women?regionType=countries&region=usa&page=1&rank", headers=headers)
+athletes_100m_data = athletes('100m')
+athletes_200m_data = athletes('200m')
+athletes_400m_data = athletes('400m')
+athletes_800m_data = athletes('8000m')
 
-doc_800m = BeautifulSoup(html_800m.text, 'html.parser')
-athletes_800m_raw = doc_800m.select('.table-row--hover')[:5]
-athletes_800m_data = []
-
-for athlete in athletes_800m_raw:
-    rank = athlete.select_one('td[data-th="Rank"]').get_text(strip=True)
-    name = athlete.select_one('td[data-th="Competitor"]').get_text(strip=True)
-    dob = athlete.select_one('td[data-th="DOB"]').get_text(strip=True)
-
-    athlete_info = {
-        'Rank': rank,
-        'Name': name,
-        'DOB': dob
-    }
-
-    athletes_800m_data.append(athlete_info)
 
 breakpoint()
