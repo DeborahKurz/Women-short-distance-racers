711f3bcf5f09cd878d5364160d221d43408e1c07
Deborah Kurz <DeborahKurz6@gmail.com>
2024-10-31 11:52:02 -0600

    MUI and Emotion installed and test components created.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+// import { Routes, Route } from "react-router-dom";
+import { AppBar, Typography } from '@mui/material';
+import AthleteCard from "./AthleteCard";
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(){
+    const [athletesList, setAthletesList] = useState([])
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    console.log(athletesList);
+
+    useEffect(()=>{
+        fetch("http://127.0.0.1:5000/")
+        .then(r=>r.json())
+        .then((athletes) => {setAthletesList(athletes)})
+        .catch((error) => console.error(error))
+    },[])
+    
+
+    return (
+        <>
+          <AppBar>
+            <Typography>Know Your Athletes</Typography>
+          </AppBar>
+            <AthleteCard athletesList={athletesList}/>
+        </>
+    )
 }
 
 export default App
---
+++ client/src/AthleteCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Paper, Card, Button } from '@mui/material';
+
+function AthleteCard( athletesList ){
+    console.log("AthleteCard:", athletesList)
+    return(
+        <Paper>
+            <Card>Card</Card>
+            <Button>Button</Button>
+        </Paper>
+    )
+}
+
+export default AthleteCard;
